fc415690f393eff17cf8392f586edc7158773552
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-09-19 08:56:41 -0400

    Change logic for ne news

--- .github/workflows/check-news.py
+++ .github/workflows/check-news.py
@@ -1,8 +1,6 @@
 """Check if the PR has a news item.
 
-Put a warning comment and fail the CI if it doesn't.
-
-Code modified from https://github.com/xonsh/xonsh
+Put a warning comment if it doesn't.
 """
 
 import os
@@ -11,17 +9,15 @@ from fnmatch import fnmatch
 from github import Github, PullRequest
 
 
-def check_news_file(pr: PullRequest.PullRequest):
-    # Get the list of files changed in the PR
-    changed_files = pr.get_files()
-    pattern = 'news/*.rst'
-    for file in changed_files:
-        if (
-            file.status == 'added' and
-            fnmatch(file.filename, pattern)
-        ):
-            return True
-    return False
+def get_added_files(pr: PullRequest.PullRequest):
+    print(pr, pr.number)
+    for file in pr.get_files():
+        if file.status == "added":
+            yield file.filename
+
+
+def check_news_file(pr):
+    return any(map(lambda file_name: fnmatch(file_name, "news/*.rst"), get_added_files(pr)))
 
 
 def get_pr_number():
@@ -45,18 +41,20 @@ def main():
     has_news_added = check_news_file(pr)
     old_comment = get_old_comment(pr)
 
-    if old_comment:
-        print("Found an existing comment from bot")
-        if has_news_added:
-            print("Delete warning from bot, since news items is added.")
+    if has_news_added:
+        if old_comment:
+            print("Found an existing comment from bot")
+            print("Delete warning from bot, since news item is added.")
             old_comment.delete()
-    elif not has_news_added:
+    else:
         print("No news item found")
-
-        pr.create_issue_comment(
-            """\
-**Warning!** No news item is found for this PR. If this is an user facing change/feature/fix,
- please add a news item by copying the format from `news/TEMPLATE.rst`.
+        if old_comment:
+            print("Old warning remains relevant, no action needed.")
+        else:
+            pr.create_issue_comment(
+                """\
+**Warning!** No news item is found for this PR. If this is a user-facing change/feature/fix,
+please add a news item by copying the format from `news/TEMPLATE.rst`.
 """
         )
         assert False
